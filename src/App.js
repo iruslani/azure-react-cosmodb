dff6749337eedb015285343523d27f7ac62c7ce2
iruslani <iruslani75@gmail.com>
2019-09-04 20:54:49 -0700

    Add fetch with react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [hasError, setErrors] = useState(false);
+  const [message, setMessage] = useState("");
+
+  async function fetchData() {
+    const res = await fetch("/api/message");
+    res
+      .json()
+      .then(res => setMessage(res))
+      .catch(err => setErrors(err));
+  }
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetchData();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{message}</h1>
+        <span>Has Errors: {JSON.stringify(hasError)}</span>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +39,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
