01e09112e40494ef8f80c538135e4d335e440ec8
iruslani <iruslani75@gmail.com>
2019-09-17 20:58:21 -0700

    Added sample fetch to spoonaular api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 const dotenv = require('dotenv');
+const fetch = require('node-fetch');
 
+// Get spoonacular apiKey
 dotenv.config();
 const apiKey = process.env.SPOONACULAR_API_KEY;
 console.log(apiKey);
 
-router.get('/message', function(req, res, next) {
+router.get('/meatball', (req, res) => {
+  const keyword = 'meatball';
+  const url = `https://api.spoonacular.com/recipes/search?query=${keyword}&apiKey=${apiKey}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      res.send({ data });
+    })
+    .catch(err => {
+      res.redirect('/error');
+    });
+});
+
+router.get('/message', (req, res, next) => {
   res.json('Welcome To Azure React CosmoDB');
 });
 
